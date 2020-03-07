58933fb8c07983a85e303e41a40469d4ea432740
AdrianMendezVargas <velociraptor088@gmail.com>
2020-03-06 21:11:22 -0400

    Agregando Ventana de Registro de Persona. Agregando estilo "TextBoxInError" a "App.axml".  Agregando la clase "CamposObligatorios" a UI/ReglasDeValidacion/

---
+++ UI/RegistroPersona.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RegistroPersona.UI {
+    /// <summary>
+    /// Interaction logic for RegistroPersona.xaml
+    /// </summary>
+    public partial class RegistroPersona : Window {
+        public RegistroPersona() {
+            InitializeComponent();
+        }
+
+        private void NuevoButton_Click(object sender , RoutedEventArgs e) {
+
+        }
+
+        private void GuardarButton_Click(object sender , RoutedEventArgs e) {
+
+        }
+
+        private void EliminarButton_Click(object sender , RoutedEventArgs e) {
+
+        }
+
+        private void BuscarButton_Click(object sender , RoutedEventArgs e) {
+
+        }
+    }
+}
---
+++ UI/ReglasDeValidacion/CamposObligatoriosRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+
+namespace RegistroPersona.UI.ReglasDeValidacion {
+    
+
+    public class CamposObligatoriosRule : ValidationRule {
+
+        public CamposObligatoriosRule() { }
+
+        public override ValidationResult Validate(object value , CultureInfo cultureInfo) {
+
+
+            try {
+
+                if (string.IsNullOrWhiteSpace(((string) value))) {
+                    return new ValidationResult(false , $"Este campo no puede estar vació");
+                }
+
+            } catch (System.Exception e) {
+
+                return new ValidationResult(false , $"error de formato.\n" + e.Message);
+            }
+
+            return ValidationResult.ValidResult;
+
+        }
+    }
+}
