c0de664c3664c761b690fb6f34431afabfd3e2a5
AdrianMendezVargas <velociraptor088@gmail.com>
2020-03-06 20:39:22 -0400

    Agregando la clase "Contexto" a DAL, e instalando EntityFrameworCore.Sqlite y .Tools

---
+++ DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroPersona.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroPersona.DAL {
+    public class Contexto : DbContext{
+
+        public DbSet<Persona> Personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlite("Data Source = RegistroPersonas.db");
+        }
+    }
+}
